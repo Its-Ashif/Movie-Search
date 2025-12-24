97c9eb348b0bb261602458cc773784d4dc76fc5b
SK MD ASHIF <60434816+Its-Ashif@users.noreply.github.com>
2025-12-24 12:42:04 +0530

    Fix image source path for hero banner

--- Archive/src/App.jsx
+++ Archive/src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
 
             <div className="wrapper overflow-x-hidden">
                 <header>
-                    <img src="../public/hero.png" alt="hero banner" />
+                    <img src="/hero.png" alt="hero banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
