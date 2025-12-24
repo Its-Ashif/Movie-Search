cd5fcdc072f80fc395e8e7452ecf4cf78235bad3
SK MD ASHIF <60434816+Its-Ashif@users.noreply.github.com>
2025-12-24 12:42:49 +0530

    Update search image source path

--- Archive/src/components/search.jsx
+++ Archive/src/components/search.jsx
@@ -4,7 +4,7 @@ const Search = ({searchTerm, setSearchTerm}) => {
     return (
         <div className="search">
             <div>
-                <img className="searchImg" src="../../public/search.svg" alt="search"/>
+                <img className="searchImg" src="/search.svg" alt="search"/>
                 <input type="text" placeholder="Search through thousands of movies"
                        value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             </div>
